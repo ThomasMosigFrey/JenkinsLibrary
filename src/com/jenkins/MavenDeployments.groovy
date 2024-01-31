640e368224294c8375c24775cfe61891788741f5
ThomasMosigFrey <tgoams@mosig-frey.de>
2024-01-31 14:29:00 +0100

    add bild Discarder

--- src/com/jenkins/MavenDeployments.groovy
+++ src/com/jenkins/MavenDeployments.groovy
@@ -10,7 +10,7 @@ public class MavenDeployments {
         this.script = script
     }
 
-    def deployToNexus(String host, String port, String creds) {
+    def deployToJBoss(String host, String port, String creds) {
         script.withCredentials([usernamePassword(credentialsId: creds, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
             script.sh "mvn deploy -Dmaven.install.skip=true -Dmaven.resources.skip=true -Dmaven.compile.skip=true -Dmaven.testResources.skip=true -Dmaven.testCompile.skip=true -Dmaven.test.skip=true -Ddeploy.jboss.host=" + host + " -Ddeploy.jboss.port=" + port + " -Ddeploy.jboss.user=${USERNAME} -Ddeploy.jboss.password=${PASSWORD}"
         }
