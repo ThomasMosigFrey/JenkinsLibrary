3b50a5e4fe467e5101c2968c345d593f1994f77b
ThomasMosigFrey <tgoams@mosig-frey.de>
2024-01-31 14:30:51 +0100

    add bild Discarder

--- src/com/jenkins/MavenDeployments.groovy
+++ src/com/jenkins/MavenDeployments.groovy
@@ -11,7 +11,7 @@ public class MavenDeployments {
     }
 
     def deployToJBoss(String host, String port, String creds) {
-        script.withCredentials([usernamePassword(credentialsId: creds, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+        script.withCredentials([script.usernamePassword(credentialsId: creds, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
             script.sh "mvn deploy -Dmaven.install.skip=true -Dmaven.resources.skip=true -Dmaven.compile.skip=true -Dmaven.testResources.skip=true -Dmaven.testCompile.skip=true -Dmaven.test.skip=true -Ddeploy.jboss.host=" + host + " -Ddeploy.jboss.port=" + port + " -Ddeploy.jboss.user=${USERNAME} -Ddeploy.jboss.password=${PASSWORD}"
         }
     }
