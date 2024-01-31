9841957ccbcde7235571be557bf543a066a2d10e
ThomasMosigFrey <tgoams@mosig-frey.de>
2024-01-31 13:48:57 +0100

    add bild Discarder

--- src/com/jenkins/MavenBuild.groovy
+++ src/com/jenkins/MavenBuild.groovy
@@ -9,7 +9,8 @@ public class MavenBuild {
 
     static def deployToNexus(def steps, def host, def port, def creds) {
         steps.withCredentials([usernamePassword(credentialsId: creds, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        steps.sh "mvn deploy -Dmaven.install.skip=true -Dmaven.resources.skip=true -Dmaven.compile.skip=true -Dmaven.testResources.skip=true -Dmaven.testCompile.skip=true -Dmaven.test.skip=true -Ddeploy.jboss.host="+host+" -Ddeploy.jboss.port="+port+" -Ddeploy.jboss.user=${USERNAME} -Ddeploy.jboss.password=${PASSWORD}"
+            steps.sh "mvn deploy -Dmaven.install.skip=true -Dmaven.resources.skip=true -Dmaven.compile.skip=true -Dmaven.testResources.skip=true -Dmaven.testCompile.skip=true -Dmaven.test.skip=true -Ddeploy.jboss.host=" + host + " -Ddeploy.jboss.port=" + port + " -Ddeploy.jboss.user=${USERNAME} -Ddeploy.jboss.password=${PASSWORD}"
+        }
     }
 
     static def callMaven(def steps, def params) {
