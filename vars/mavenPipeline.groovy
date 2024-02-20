6693fb894b54df4b189dffe11d5357548e5ff938
ThomasMosigFrey <tgoams@mosig-frey.de>
2024-02-20 16:50:26 +0100

    init

--- vars/mavenPipeline.groovy
+++ vars/mavenPipeline.groovy
@@ -9,48 +9,48 @@ def call(body) {
     body.delegate = config
     body()
     
-    pipeline {
-      agent { label 'Demo' }
-      stages {
-          stage('Compile/Test') {
-              steps {
-                  script {
-                      echo config.timeout
-                      echo config.mavenVersion
-                      mvnBuild.cleanInstall(this)
-                  }
-              }
-          }
-          stage('Report') {
-              steps {
-                  junit allowEmptyResults: true, testResults: '**/*.xml'
-              }
-          }
-          stage('Source Code Check') {
-              steps {
-                  withMaven(maven: 'Maven 3.8.6') {
-                      withSonarQubeEnv(credentialsId: 'e33b1bc1-7530-4989-b282-d348ac73bc27', installationName: 'sonar') { // You can override the credential to be used
-                          sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
+        pipeline {
+          agent { label 'Demo' }
+          stages {
+              stage('Compile/Test') {
+                  steps {
+                      script {
+                          echo config.timeout
+                          echo config.mavenVersion
+                          mvnBuild.cleanInstall(this)
                       }
                   }
               }
-          }
-          stage('Deploy') {
-              steps {
-                  withMaven(maven: 'Maven 3.8.6') {
-                      echo "deploy"
+              stage('Report') {
+                  steps {
+                      junit allowEmptyResults: true, testResults: '**/*.xml'
                   }
               }
-          }
-     }
-     post {
-         always {
-             sh 'printenv'
-             emailext body: '', recipientProviders: [developers()], subject: 'Build executed ', to: 'thomas@mosig-frey.de'
+              stage('Source Code Check') {
+                  steps {
+                      withMaven(maven: 'Maven 3.8.6') {
+                          withSonarQubeEnv(credentialsId: 'e33b1bc1-7530-4989-b282-d348ac73bc27', installationName: 'sonar') { // You can override the credential to be used
+                              sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
+                          }
+                      }
+                  }
+              }
+              stage('Deploy') {
+                  steps {
+                      withMaven(maven: 'Maven 3.8.6') {
+                          echo "deploy"
+                      }
+                  }
+              }
          }
-         success {
-              cleanWs()
+         post {
+             always {
+                 sh 'printenv'
+                 emailext body: '', recipientProviders: [developers()], subject: 'Build executed ', to: 'thomas@mosig-frey.de'
+             }
+             success {
+                  cleanWs()
+             }
          }
-     }
-  }
+      }
 }
--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -7,28 +7,43 @@ def call(body) {
     body()
 
     pipeline {
-        agent { node { label 'demo' } }
-
-        triggers { pollSCM('H * * * *') }
-
+        agent {
+            label 'linux'
+        }
         stages {
-            stage('Compile/Test') {
+            stage ('compile/test') {
                 steps {
-                    withMaven(maven: 'Maven 3.3.9') {
-                        echo "mvn clean install"
+                    withMaven(globalMavenSettingsConfig: 'ae44f8b3-3bf7-4624-8e87-74659f3f817f', maven: 'maven3', mavenSettingsConfig: '', traceability: true) {
+                        sh "mvn clean package"
                     }
-                    archiveArtifacts allowEmptyArchive: true, artifacts: 'target/*.jar', followSymlinks: false, onlyIfSuccessful: true
                 }
             }
-        }
-        post {
-            success {
-                // One or more steps need to be included within each condition's block.
-                echo "Yeah!!"
+            stage('deploy to nexus') {
+                steps {
+                    withMaven(globalMavenSettingsConfig: 'ae44f8b3-3bf7-4624-8e87-74659f3f817f', maven: 'maven3', mavenSettingsConfig: '', traceability: true) {
+                        sh "mvn install -DskipTests"
+                    }
+                }
             }
-            unsuccessful {
-                // One or more steps need to be included within each condition's block.
-                echo "email ..."
+            stage('deploy to jboss to uat test system') {
+                when { branch 'master'}
+                steps {
+                    withMaven(globalMavenSettingsConfig: 'ae44f8b3-3bf7-4624-8e87-74659f3f817f', maven: 'maven3', mavenSettingsConfig: '', traceability: true) {
+                        withCredentials([usernamePassword(credentialsId: 'bde764f8-d98e-411f-9a24-900dccac6d04', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+                            sh "mvn deploy -DskipTests -Ddeploy.jboss.host=10.10.60.63 -Ddeploy.jboss.port=10090 -Ddeploy.jboss.user=${USERNAME} -Ddeploy.jboss.password=${PASSWORD}"
+                        }
+                    }
+                }
+            }
+            stage('deploy to jboss to dev test system') {
+                when { branch 'dev'}
+                steps {
+                    withMaven(globalMavenSettingsConfig: 'ae44f8b3-3bf7-4624-8e87-74659f3f817f', maven: 'maven3', mavenSettingsConfig: '', traceability: true) {
+                        withCredentials([usernamePassword(credentialsId: '24dfea8d-7135-4d8b-908f-c0f74088e116', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+                            sh "mvn deploy -DskipTests -Ddeploy.jboss.host=10.10.60.63 -Ddeploy.jboss.port=10090 -Ddeploy.jboss.user=${USERNAME} -Ddeploy.jboss.password=${PASSWORD}"
+                        }
+                    }
+                }
             }
         }
     }
