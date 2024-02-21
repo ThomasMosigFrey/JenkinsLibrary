00e9b5848de9b8a83146b5187aa54ed3cbcce91c
ThomasMosigFrey <tgoams@mosig-frey.de>
2024-02-21 08:43:35 +0100

    init

--- vars/PipelineWithClosure.groovy
+++ vars/PipelineWithClosure.groovy
@@ -1,11 +1,10 @@
 import com.jenkins.*
 
 def call(Closure body) {
-    // ingest configs from jobs
+    // ingest configs from from job's Closure
     LinkedHashMap config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
-
     body()
 
     echo "key: ${config.key}, value: ${config.value}."
