65ad6f3b99cd3d92a2d136b24fc8346acc2b0742
ThomasMosigFrey <tgoams@mosig-frey.de>
2024-02-21 08:46:55 +0100

    init

--- vars/PipelineWithParameters.groovy
+++ vars/PipelineWithParameters.groovy
@@ -4,4 +4,16 @@ def call(String key = 'default', int value = 0) {
     echo "key: ${key}, value: ${value}."
 
     // inject your pipeline here
+    pipeline {
+        agent { label 'Parameters Demo' }
+        stages {
+            stage('Compile/Test') {
+                steps {
+                    script {
+                        echo "Hello World"
+                    }
+                }
+            }
+        }
+    }
 }
