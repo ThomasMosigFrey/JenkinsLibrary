9b9f73e85320a763938d5f740a014582599309d1
ThomasMosigFrey <tgoams@mosig-frey.de>
2024-02-21 09:34:15 +0100

    init

--- vars/PipelineWithParameters.groovy
+++ vars/PipelineWithParameters.groovy
@@ -1,9 +1,9 @@
 import com.jenkins.*
 
-def call(String agent = 'linux', String key = 'default', int value = 0) {
+def call(String host = 'linux', String key = 'default', int value = 0) {
     // inject your pipeline here
     pipeline {
-        agent { label "${agent}" }
+        agent { label "${host}" }
         stages {
             stage('Parameters Demo') {
                 steps {
