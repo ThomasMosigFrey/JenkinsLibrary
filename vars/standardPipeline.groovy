f32668e3563dc2610974244e9d5e704213a5e3b1
ThomasMosigFrey <tgoams@mosig-frey.de>
2024-08-22 12:14:03 +0200

    init

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -6,12 +6,19 @@ def call(Closure body) {
     body.delegate = config
     body()
 
+    if(!config.maven) {
+        config.maven = "maven3"
+    }
+    if(!config.jbossHost) {
+        config.jbossHost = "10.10.60.59"
+    }
+
     pipeline {
         agent {
-            label 'linux'
+            label ${config.label}
         }
         environment {
-            NEXUS_HOST = "10.10.60.59"
+            NEXUS_HOST = config.nexusHost
         }
         options {
           buildDiscarder logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '5', daysToKeepStr: '', numToKeepStr: '5')
@@ -19,20 +26,18 @@ def call(Closure body) {
           retry(conditions: [agent()], count: 3)
           timeout(time: 1, unit: 'HOURS')
         }
-        
-        parameters { choice(name: 'jboss_server', choices: ['10.10.60.59', '10.10.60.58'], description: 'jboss server to deploy to') }
 
         stages {
             stage ('compile/test') {
                 steps {
-                    withMaven(globalMavenSettingsConfig: 'ae44f8b3-3bf7-4624-8e87-74659f3f817f', maven: 'maven3', mavenSettingsConfig: '', traceability: true) {
+                    withMaven(globalMavenSettingsConfig: 'ae44f8b3-3bf7-4624-8e87-74659f3f817f', maven: ${config.maven}, mavenSettingsConfig: '', traceability: true) {
                         sh "mvn clean package"
                     }
                 }
             }
             stage('deploy to nexus') {
                 steps {
-                    withMaven(globalMavenSettingsConfig: 'ae44f8b3-3bf7-4624-8e87-74659f3f817f', maven: 'maven3', mavenSettingsConfig: '', traceability: true) {
+                    withMaven(globalMavenSettingsConfig: 'ae44f8b3-3bf7-4624-8e87-74659f3f817f', maven: ${config.maven}, mavenSettingsConfig: '', traceability: true) {
                         sh "mvn install -DskipTests"
                     }
                 }
@@ -40,9 +45,9 @@ def call(Closure body) {
             stage('deploy to jboss to uat test system') {
                 when { branch 'master'}
                 steps {
-                    withMaven(globalMavenSettingsConfig: 'ae44f8b3-3bf7-4624-8e87-74659f3f817f', maven: 'maven3', mavenSettingsConfig: '', traceability: true) {
+                    withMaven(globalMavenSettingsConfig: 'ae44f8b3-3bf7-4624-8e87-74659f3f817f', maven: ${config.maven}, mavenSettingsConfig: '', traceability: true) {
                         withCredentials([usernamePassword(credentialsId: '1cbbdb5b-fc28-4cd0-8e7b-698a55743423', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-                            sh "mvn deploy -DskipTests -Ddeploy.jboss.host=${params.jboss_server} -Ddeploy.jboss.port=10090 -Ddeploy.jboss.user=${USERNAME} -Ddeploy.jboss.password=${PASSWORD}"
+                            sh "mvn deploy -DskipTests -Ddeploy.jboss.host=${config.jbossHost} -Ddeploy.jboss.port=10090 -Ddeploy.jboss.user=${USERNAME} -Ddeploy.jboss.password=${PASSWORD}"
                         }
                     }
                 }
@@ -50,9 +55,9 @@ def call(Closure body) {
             stage('deploy to jboss to dev test system') {
                 when { branch 'dev'}
                 steps {
-                    withMaven(globalMavenSettingsConfig: 'ae44f8b3-3bf7-4624-8e87-74659f3f817f', maven: 'maven3', mavenSettingsConfig: '', traceability: true) {
+                    withMaven(globalMavenSettingsConfig: 'ae44f8b3-3bf7-4624-8e87-74659f3f817f', maven: ${config.maven}, mavenSettingsConfig: '', traceability: true) {
                         withCredentials([usernamePassword(credentialsId: '1cbbdb5b-fc28-4cd0-8e7b-698a55743423', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-                            sh "mvn deploy -DskipTests -Ddeploy.jboss.host=${params.jboss_server} -Ddeploy.jboss.port=10090 -Ddeploy.jboss.user=${USERNAME} -Ddeploy.jboss.password=${PASSWORD}"
+                            sh "mvn deploy -DskipTests -Ddeploy.jboss.host=${config.jbossHost} -Ddeploy.jboss.port=10090 -Ddeploy.jboss.user=${USERNAME} -Ddeploy.jboss.password=${PASSWORD}"
                         }
                     }
                 }
