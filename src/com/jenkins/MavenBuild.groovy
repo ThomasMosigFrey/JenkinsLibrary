d16ea41f95eb558885b563f76e33fa14a93e8b70
ThomasMosigFrey <tgoams@mosig-frey.de>
2024-08-22 15:04:43 +0200

    init

--- src/com/jenkins/MavenBuild.groovy
+++ src/com/jenkins/MavenBuild.groovy
@@ -6,12 +6,12 @@ import hudson.*;
 public class MavenBuild {
     def mavenTool  = 'maven3'
     def settings = ""
-    def scripts
+    def script
 
-    public MavenBuild(def scripts, def mavenTool, def settings) {
+    public MavenBuild(def script, def mavenTool, def settings) {
         this.mavenTool = mavenTool
         this.settings = settings
-        this.scripts = scripts
+        this.script = script
     }
 
     def compile() {
@@ -27,13 +27,15 @@ public class MavenBuild {
     }
 
     def deploy(def serverAddress, def credId) {
-        this.scripts.withCredentials([this.scripts.usernamePassword(credentialsId: credId, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-            callMaven("deploy -Dmaven.test.skip=true -Ddeploy.jboss.host="+serverAddress+ " -Ddeploy.jboss.port=10090 -Ddeploy.jboss.user=${USERNAME} -Ddeploy.jboss.password=${PASSWORD}")
+        script {
+            withCredentials([usernamePassword(credentialsId: credId, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+                callMaven("deploy -Dmaven.test.skip=true -Ddeploy.jboss.host="+serverAddress+ " -Ddeploy.jboss.port=10090 -Ddeploy.jboss.user=${env.USERNAME} -Ddeploy.jboss.password=${env.PASSWORD}")
+            }
         }
     }
 
     def callMaven(def params) {
-        this.scripts.withMaven(globalMavenSettingsConfig: this.settings, maven: this.mavenTool) {
+        this.script.withMaven(globalMavenSettingsConfig: this.settings, maven: this.mavenTool) {
             this.scripts.sh 'mvn ' + params
         }
     }
