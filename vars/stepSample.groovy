407f26b339937502e0ab2f7ba6b14fd878784445
ThomasMosigFrey <tgoams@mosig-frey.de>
2024-10-03 13:20:31 +0200

    init

--- vars/stepSample.groovy
+++ vars/stepSample.groovy
@@ -1,8 +1,15 @@
 import com.jenkins.*
 
+String name
+
+public stepSample(String name) {
+    this.name =name;
+}
+
 def call(Closure body) {
     echo body.toString()
     print body.getMaximumNumberOfParameters()
     echo "mvn clean package"
+    echo this.name
     body()
 }
